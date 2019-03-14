72eb7efe77d9d209df4c60c07800fbf0923317b9
AdrielFreud <gamerbra39@gmail.com>
2019-03-14 00:41:47 -0300

    Update LoadLibrary.c

--- LoadLibrary.c
+++ LoadLibrary.c
@@ -5,34 +5,35 @@
 # FB: http://www.facebook.com/xrn401
 #   =>DebutySecTeamSecurity<=
 ----------------------------------
+Functions Contained in dll
+
+divi = division
+soma = add
+functions = banner
+mult = for *
+sub = sub for subtraction
 */
 
-#include "ldfun.h"
+#include <stdio.h>
+#include "windows.h"
 
-typedef int (__stdcall* func_ptr_t)();
+typedef int (__stdcall* stcall_t)(void);
 
-void call_dll(char *name_dll, char* function){
+void call_dll(char *name_dll, char *function){
 
-	HMODULE dll = LoadLibrary(name_dll);
-	if(dll != NULL){
-	 	func_ptr_t func = (func_ptr_t)GetProcAddress(dll, function);
-		if(func != NULL){
-			const char result = func();
-			FreeLibrary(dll);
-		}else{
-			fatal("Nao foi possivel chamar essa funcao!");
-		}
-		FreeLibrary(dll);
-	}else{
-		fatal("Nao foi possivel carregar a DLL!");
+	HMODULE path_dll = LoadLibrary(name_dll);
+	if(path_dll == NULL){
+		exit(1);
 	}
+
+	stcall_t search = (stcall_t)GetProcAddress(path_dll, function);
+	const char result = search();
+	FreeLibrary(path_dll);
 }
 
 int main(int argc, char *argv[]){
-	if(argc < 2){
-		fatal("\n\t=> AdrielFreud <=\n\nUSAGE: root@localhost~# ./load name.dll function");
-	}else{
+	if(argc > 2)
 		call_dll(argv[1], argv[2]);
-	}
-	return EXIT_SUCCESS;
+
+	return 0;
 }
